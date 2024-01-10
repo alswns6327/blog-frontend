65c9ff9150088df01cf18d66f26473b2d5fdf34a
alswns6327 <alswns6327@naver.com>
2024-01-10 17:14:28 +0900

    기본 작업 환경 세팅

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from './modules/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from '../node_modules/react-redux/dist/react-redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Button from '../components/common/Button';
 
 const PostListPage = () => {
-  return <div>2</div>;
+  return (
+    <div>
+      2<Button>test</Button>
+    </div>
+  );
 };
 
 export default PostListPage;
