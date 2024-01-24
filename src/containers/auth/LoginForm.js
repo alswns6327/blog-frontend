14c2429e0d898f9e56d097a8cef9667c136c0e92
alswns6327 <alswns6327@naver.com>
2024-01-24 18:27:35 +0900

    create login form, update auth redux

---
+++ src/containers/auth/LoginForm.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({
+    form: auth.login,
+  }));
+  // input onChange Handler
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'login',
+        key: name,
+        value,
+      }),
+    );
+  };
+
+  // from submit handler
+  const onSubmit = (e) => {
+    e.preventDefault();
+    //
+  };
+
+  // initialize
+  useEffect(() => {
+    dispatch(initializeForm('login'));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default LoginForm;
