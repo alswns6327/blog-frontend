58a61eca5227e2d2a82ae72f5eb57c3505c1b875
alswns6327 <alswns6327@naver.com>
2024-02-07 18:29:45 +0900

    header 추가

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 const PostListPage = () => {
   return (
-    <div>
-      2<Button>test</Button>
-    </div>
+    <>
+      <Header />
+      <div>안녕하세요</div>
+    </>
   );
 };
 
