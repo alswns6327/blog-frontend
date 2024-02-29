ee1f069d9b197c490d499e4f793a649cb19e3474
alswns6327 <alswns6327@naver.com>
2024-02-29 18:38:50 +0900

    viewer

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import HeaderContainer from '../components/common/HeaderContainer';
+import PostViewer from '../components/post/PostViewer';
 
 const PostPage = () => {
-  return <div>3</div>;
+  return (
+    <>
+      <HeaderContainer />
+      <PostViewer />
+    </>
+  );
 };
 
 export default PostPage;
